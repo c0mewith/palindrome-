f55b018a2e5668b8a5fb07c6f1a67561cce2796c
c0mewith <79433798+c0mewith@users.noreply.github.com>
2021-03-08 19:43:28 +0900

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,1 +1,37 @@
-﻿
+﻿#include <stdio.h>
+#include <string.h>
+
+
+void palin(char* arr, int len);
+
+int main(void)
+{
+	char ary[30];
+	int leng;
+
+	scanf("%s", ary);
+	leng = strlen(ary);
+	palin(ary, leng);
+	return 0;
+
+	
+
+}
+
+void palin(char* arr, int len)
+{
+	int i;
+	int j = len - 1;
+	for (i = 0; i < len / 2; i++)
+	{
+		if (arr[i] != arr[j])
+		{
+			printf("this word is not palindrome!");
+			return;
+		}
+		j--;
+	}
+	printf("this word is palindrome!");
+
+	return;
+}
